8b9a10af7556eae35e997be5fbf48cd12d082f27
Spyrosskordos <spyrosskordos@gmail.com>
2023-04-29 20:35:23 +0300

    created InstantiationType

---
+++ Sources/DependencyInjection/InstantiationType.swift
@@ -0,0 +1,14 @@
+//
+//  InstantiationType.swift
+//  SwimPal
+//
+//  Created by Bruno Benčević on 24.04.2023..
+//
+
+import Foundation
+
+enum InstantiationType {
+    case singleton
+    case newInstance
+    case automatic
+}
