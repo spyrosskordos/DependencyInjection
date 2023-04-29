241a3902f222c3016f4463f8858caa6c7a9cf596
Spyrosskordos <spyrosskordos@gmail.com>
2023-04-29 21:01:35 +0300

    dependency container

--- Sources/DependencyInjection/Dependency.swift
+++ Sources/DependencyInjection/Dependency.swift
@@ -1,6 +1,6 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by Spyros Skordos Sko on 29/4/23.
 //
@@ -9,14 +9,17 @@ import Foundation
 
 @propertyWrapper
 struct Dependency<Dependency> {
-    
+
     var dependency: Dependency
-    
+
     init(_ instantiationType: InstantiationType = .newInstance) {
-        //TODO 
+        guard let dependency = DependencyContainer.resolve(dependencyType: instantiationType, Dependency.self)
+        else {
+            fatalError("No dependency of type \(String(describing: Dependency.self)) registered!")
+        }
         self.dependency = dependency
     }
-    
+
     var wrappedValue: Dependency {
         get { self.dependency }
         mutating set { dependency = newValue }
